4ea8a70505251bbfbf9c5a11794b7d609ee09bd1
mtchllbrrn <mitchell.stocky@gmail.com>
2014-08-14 17:51:40 -0500

    Added infinite loop, sleep timer: 60 seconds

--- test_bot.py
+++ test_bot.py
@@ -1,15 +1,17 @@
 import praw
+import time
 
 r = praw.Reddit(user_agent = "test bot, /u/letsLOVE")
 r.login("munx1erbot", "thirdverse")
 subreddit = r.get_subreddit("test")
 
+# Would be nice if already_replied was a text file that persisted even if bot crashed.
 already_replied = []
 
 comments_gen = subreddit.get_comments(limit=10)
-for comment in comments_gen:
-	if ("critical mass" in comment.body.lower()) and (comment.submission not in already_replied):
-		comment.reply("Fuck Critical Mass!" + u'\u2122')
-		already_replied.append(comment.submission)
-		for i in already_replied:
-			print i
+while True:
+	for comment in comments_gen:
+		if ("critical mass" in comment.body.lower()) and (comment.submission not in already_replied):
+			comment.reply("Fuck Critical Mass!" + u'\u2122')
+			already_replied.append(comment.submission)
+	time.sleep(60)
