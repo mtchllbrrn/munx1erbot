e9749be12f59cb1351ca60e7449709dec882af33
mtchllbrrn <mitchell.stocky@gmail.com>
2014-08-14 17:43:20 -0500

    updated, should be close to finished.

--- test_bot.py
+++ test_bot.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
 import praw
 
 r = praw.Reddit(user_agent = "test bot, /u/letsLOVE")
-already_replied = []
-r.login("munx1erbot", "cRypto6pIe1eNgage5sKy")
-
+r.login("munx1erbot", "thirdverse")
 subreddit = r.get_subreddit("test")
+
+already_replied = []
 
 comments_gen = subreddit.get_comments(limit=10)
 for comment in comments_gen:
-	if "thanks" in comment.body.lower():
-		print comment.body
-		comment.reply(u'\2122')
+	if ("critical mass" in comment.body.lower()) and (comment.submission not in already_replied):
+		comment.reply("Fuck Critical Mass!" + u'\u2122')
+		already_replied.append(comment.submission)
+		for i in already_replied:
+			print i
